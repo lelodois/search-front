3c5e9e4da62e9a098b721279308cf621a38d78d1
Léo José da Costa <leoeduar@gmail.com>
2018-12-12 23:13:48 -0200

    Update url-util.service.ts

--- src/app/provider/url-util.service.ts
+++ src/app/provider/url-util.service.ts
@@ -1,3 +1,10 @@
+// Execução dentro do docker
 export const URL_SEARCH = 'http://172.18.0.30:8981/users';
 export const URL_USER = 'http://172.18.0.20:8982/user';
 export const URL_LOGIN = 'http://172.18.0.20:8982/oauth/token?grant_type=password';
+
+// Execução local
+// export const URL_SEARCH = 'http://localhost:8981/users';
+// export const URL_USER = 'http://localhost:8982/user';
+// export const URL_LOGIN = 'http://localhost:8982/oauth/token?grant_type=password';
+
